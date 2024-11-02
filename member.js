d65f80ad049bfe7cd56549ff93840ee363af5834
Konpaku Youmu <thyoumu8848@gmail.com>
2024-11-02 20:14:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    return {
+        name: 'skillsMember',
+        type: 'member',
+        value: ['JavaScript', 'HTML', 'CSS', 'Python', 'Ruby', 'C++', 'Java', 'C#', 'PHP', 'SQL', 'Swift', 'Go', 'TypeScript', 'R', 'Kotlin', 'Scala', 'Perl', 'Rust', 'Haskell', 'Erlang', 'Clojure', 'F#', 'Lua', 'Elixir', 'Dart', 'Julia', 'Eiffel', 'Tcl', 'Ada', 'Pascal', 'Delphi', 'Fortran', 'COBOL', 'Lisp', 'Scheme', 'Prolog', 'Smalltalk', 'Haskell', 'Erlang', 'Clojure', 'F#', 'Lua', 'Elixir', 'Dart', 'Julia', 'Eiffel', 'Tcl', 'Ada', 'Pascal', 'Delphi', 'Fortran', 'COBOL', 'Lisp', 'Scheme', 'Prolog', 'Smalltalk', 'Haskell', 'Erlang', 'Clojure', 'F#', 'Lua', 'Elixir', 'Dart', 'Julia', 'Eiffel', 'Tcl', 'Ada', 'Pascal', 'Delphi', 'Fortran', 'COBOL', 'Lisp', 'Scheme', 'Prolog', 'Smalltalk', 'Haskell', 'Erlang', 'Clojure', 'F#', 'Lua', 'Elixir', 'Dart', 'Julia', 'Eiffel', 'Tcl', 'Ada', 'Pascal', 'Delphi', 'Fortran', 'COBOL', 'Lisp', 'Scheme', 'Prolog', 'Smalltalk']
+    };
+}
